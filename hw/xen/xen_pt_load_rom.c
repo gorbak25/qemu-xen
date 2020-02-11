4b811965ab59dd497658e275125c24224e4b872f
Grzegorz Uriasz <gorbak25@gmail.com>
2020-02-11 17:59:16 +0000

    Ignore host VBIOS

--- hw/xen/xen_pt_load_rom.c
+++ hw/xen/xen_pt_load_rom.c
@@ -36,6 +36,21 @@ void *pci_assign_dev_load_option_rom(PCIDevice *dev,
         return NULL;
     }
 
+    //TODO: when runnning inside a stubdom retrieve the rombios from a vchan
+    // For now just always return a fake invalid vbios :P
+    int size_vbios = 131072; // Hadcoded size for my laptop - this is ok as this is a test to see what else will break
+    snprintf(name, sizeof(name), "%s.rom", object_get_typename(owner));
+    memory_region_init_ram(&dev->rom, owner, name, size_vbios, &error_abort);
+    ptr = memory_region_get_ram_ptr(&dev->rom);
+    memset(ptr, 0xff, size_vbios);
+
+    // This will trick qemu into mapping a dummy vbios into the guest
+    // This won't matter because as soon as the i935 driver will take over the device
+    // the vbios won't be needed
+    *size = size_vbios;
+    error_report("ASDF: %p", ptr);
+    return ptr;
+
     snprintf(rom_file, sizeof(rom_file),
              "/sys/bus/pci/devices/%04x:%02x:%02x.%01x/rom",
              domain, bus, slot, function);
@@ -59,15 +74,6 @@ void *pci_assign_dev_load_option_rom(PCIDevice *dev,
     }
     fseek(fp, 0, SEEK_SET);
 
-    snprintf(name, sizeof(name), "%s.rom", object_get_typename(owner));
-    memory_region_init_ram(&dev->rom, owner, name, st.st_size, &error_abort);
-    ptr = memory_region_get_ram_ptr(&dev->rom);
-    memset(ptr, 0xff, st.st_size);
-
-    // This will trick qemu into mapping a dummy vbios into the guest
-    // This won't matter becouse as soon as the i935 driver will take over the device
-    // the vbios won't be needed
-    *size = st.st_size;
     if (!fread(ptr, 1, st.st_size, fp)) {
         error_report("pci-assign: Cannot read from host %s", rom_file);
         error_printf("Device option ROM contents are probably invalid "
